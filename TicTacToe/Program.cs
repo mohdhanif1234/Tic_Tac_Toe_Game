e870d272e24149324e4dff51da33e69fbb4e73ed
mohdhanif1234 <hanif.topia@gmail.com>
2021-09-28 11:06:48 +0530

    [Hanif]:Added a program to create an empty array for Tic Tac Toe game

---
+++ TicTacToe/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TicTacToeGame game = new TicTacToeGame(); // Creating an object 'game' of class 'TicTacToeGame'
+            game.TicTacToeGameMethod(); // Calling a method 'TicTacToeGameMethod'
+        }
+    }
+}
---
+++ TicTacToe/TicTacToeGame.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe
+{
+    class TicTacToeGame
+    {
+        // Declaring and creating an empty array named board of size 10 characters
+        char[] board = new char[10];
+
+        public void TicTacToeGameMethod()
+        {
+
+        }
+    }
+}
